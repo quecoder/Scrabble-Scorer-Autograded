192a30b124ba3e1beb09effb0121f5f6881ceac1
Quinton Kornegay <quintonkornegay@Aleighas-MacBook-Air.local>
2023-10-19 15:43:53 -0500

    THIS IS FINAL

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -35,38 +35,6 @@ function initialPrompt() {
    return firstQuestion;
 };
 
-
-"-----------------------------------------------------------------------------------"
-
-let newPointStructure = {
-   a: 1,
-   e: 1,
-   i: 1,
-   o: 1,
-   u: 1,
-   l: 1,
-   n: 1,
-   r: 1,
-   s: 1,
-   t: 1,
-   d: 2,
-   g: 2,
-   b: 3,
-   c: 3,
-   m: 3,
-   p: 3,
-   f: 4,
-   h: 4,
-   v: 4,
-   w: 4,
-   y: 4,
-   k: 5,
-   j: 8,
-   x: 8,
-   q: 10,
-   z: 10
-};
-
 "-----------------------------------------------------------------------------------"
 
 function simpleScorer (word) {
@@ -151,6 +119,9 @@ function scorerPrompt(word) {
 };
 "-----------------------------------------------------------------------------------"
 function transform(oldPointStructure) {
+
+   let newPointStructure = {}
+
 for (let points in oldPointStructure) {
    let score = Number(points);
 
@@ -161,6 +132,7 @@ for (let points in oldPointStructure) {
 return newPointStructure;
 };
 
+let newPointStructure = transform(oldPointStructure);
 "-----------------------------------------------------------------------------------"
 function runProgram() {
    let userWord = initialPrompt();
